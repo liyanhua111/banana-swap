e352cad623ddaf7577d2948333b681bb1f665cf3
liyanhua <857727446@qq.com>
2021-06-24 18:08:04 +0800

    comingsoon

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import React from "react";
 import {Link,Route,Switch,Redirect} from 'react-router-dom'
 import "./App.less";
 import Main from "./components/main/main";
-import {Home} from "./components/home";
+import { Home } from "./components/home";
+import {ComingSoon} from "./components/comingSoon"
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route path="/home" component={Home} />
         <Route path="/swap" component={Main} />
-        <Redirect to="/home"/>
+        <Route path="/ComingSoon" component={ComingSoon} />
+        <Redirect to="/ComingSoon"/>
       </Switch>
     </div>
   );
