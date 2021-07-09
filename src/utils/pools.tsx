468c2ecbb71e8e6ba2137668bd6b5abdff6be042
iwaity <240570941@qq.com>
2021-07-09 15:38:41 +0800

    fix: change amout info

--- src/utils/pools.tsx
+++ src/utils/pools.tsx
@@ -1097,10 +1097,10 @@ function estimateProceedsFromInput(
   proceedsQuantityInPool: number,
   inputAmount: number
 ): number {
-  console.log('===================================================================')
-  console.log(`(proceedsQuantityInPool * 997 * inputAmount) / (inputQuantityInPool * 1000 + inputAmount * 997)`)
-  console.log(`(${proceedsQuantityInPool} * 997 * ${inputAmount}) / (${inputQuantityInPool} * 1000 + ${inputAmount} * 997)= `, (proceedsQuantityInPool * 997 * inputAmount) / (inputQuantityInPool * 1000 + inputAmount * 997))
-  console.log('===================================================================')
+  // console.log('===================================================================')
+  // console.log(`(proceedsQuantityInPool * 997 * inputAmount) / (inputQuantityInPool * 1000 + inputAmount * 997)`)
+  // console.log(`(${proceedsQuantityInPool} * 997 * ${inputAmount}) / (${inputQuantityInPool} * 1000 + ${inputAmount} * 997)= `, (proceedsQuantityInPool * 997 * inputAmount) / (inputQuantityInPool * 1000 + inputAmount * 997))
+  // console.log('===================================================================')
   return (
     (proceedsQuantityInPool * 997 * inputAmount) /
     (inputQuantityInPool * 1000 + inputAmount * 997)
@@ -1115,14 +1115,14 @@ function estimateInputFromProceeds(
   if (proceedsAmount >= proceedsQuantityInPool) {
     return "Not possible";
   }
-  console.log('===================================================================')
+  // console.log('===================================================================')
 
-  console.log(`(inputQuantityInPool * proceedsAmount) / (proceedsQuantityInPool - proceedsAmount)`)
-  console.log(`(${inputQuantityInPool} * ${proceedsAmount}) / (${proceedsQuantityInPool} - ${proceedsAmount})= `, (inputQuantityInPool * proceedsAmount) / (proceedsQuantityInPool - proceedsAmount))
-  console.log('===================================================================')
+  // console.log(`(inputQuantityInPool * proceedsAmount) / (proceedsQuantityInPool - proceedsAmount)`)
+  // console.log(`(${inputQuantityInPool} * ${proceedsAmount}) / (${proceedsQuantityInPool} - ${proceedsAmount})= `, (inputQuantityInPool * proceedsAmount) / (proceedsQuantityInPool - proceedsAmount))
+  // console.log('===================================================================')
   return (
-    (inputQuantityInPool * proceedsAmount) /
-    (proceedsQuantityInPool - proceedsAmount)
+    (inputQuantityInPool * 997 * proceedsAmount) /
+    (proceedsQuantityInPool * 1000 - proceedsAmount * 997)
   );
 }
 
@@ -1210,7 +1210,7 @@ export async function calculateDependentAmount(
           indBasketQuantity,
           indAdjustedAmount
         );
-        console.log(depAdjustedAmount, 'SwapGivenProceeds1')
+        // console.log(depAdjustedAmount, 'SwapGivenProceeds1')
         break;
       case PoolOperation.SwapGivenInput:
         depAdjustedAmount = estimateProceedsFromInput(
@@ -1218,7 +1218,7 @@ export async function calculateDependentAmount(
           depBasketQuantity,
           indAdjustedAmount
         );
-        console.log(depAdjustedAmount, 'SwapGivenInput2')
+        // console.log(depAdjustedAmount, 'SwapGivenInput2')
         break;
     }
   }
