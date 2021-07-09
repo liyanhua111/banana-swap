4e6fa8107121d9c1004e3a2b92ccf781a5b87f6c
iwaity <240570941@qq.com>
2021-07-09 18:29:56 +0800

    fix: new ui version

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.e0f3553209aa724f04873be871c1f6ca.js"
+  "./precache-manifest.1bed249d13e55ed14d7b2289b8aa3ea1.js"
 );
 
 self.addEventListener('message', (event) => {
