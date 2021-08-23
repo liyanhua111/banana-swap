232a08f162d7a907138b62e655a8429ddc662078
liyanhua111 <857727446@qq.com>
2021-08-23 23:36:20 +0800

    修改

--- src/redux/store 2.js
+++
@@ -1,8 +0,0 @@
-import { createStore, applyMiddleware } from "redux";
-//处理redux的异步任务的中间件
-import thunk from "redux-thunk";
-import reducer from "./reducer";
-
-const store = createStore(reducer, applyMiddleware(thunk));
-
-export default store;
