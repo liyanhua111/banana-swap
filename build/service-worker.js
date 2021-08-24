3ccdf3d94fde5c80115c56b50c8e8667bae0efdd
liyanhua111 <857727446@qq.com>
2021-08-24 18:22:20 +0800

    builf

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2bd7a8ad77829f61313bd372b5b9966d.js"
+  "./precache-manifest.f2ebf5802834791e7034ef4a6239bb2b.js"
 );
 
 self.addEventListener('message', (event) => {
