8ea72f7f210560b288cddc4375b4c84951ef677d
iwaity <email@iwaity.com>
2021-08-25 17:23:11 +0800

    fix: set price

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.68b0f6ee84e63a497434823d6e97651d.js"
+  "./precache-manifest.4ac75cf4cf867fbf96687626a87cf72e.js"
 );
 
 self.addEventListener('message', (event) => {
