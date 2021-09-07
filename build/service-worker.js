b6862bbd62c6081b8f5fd64ddf58f0395210d197
liyanhua111 <857727446@qq.com>
2021-09-07 17:09:12 +0800

    修改

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.fe795549f7d1570f1fe951c424f24493.js"
+  "./precache-manifest.a4e8aa0fc5b86738f24ca5b79ea4e361.js"
 );
 
 self.addEventListener('message', (event) => {
